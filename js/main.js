bdebfd122bd93756e39d958e966a0a7a5e433011
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-23 15:05:47 +0200

    exercise 1 completed, optional exercise start

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("OK andiamo");
+$(document).ready(function (){
+  //console.log("ok");
+  
+
+  var studente = {
+
+    nome : "Vincenzo",
+    cognome : "Laveneziana",
+    età : "26"
+  }
+
+  console.log(studente);
+  
+  for (var key in studente){
+    console.log(key + ": " + studente[key])
+  }
+
+
+
+});//fine ready
