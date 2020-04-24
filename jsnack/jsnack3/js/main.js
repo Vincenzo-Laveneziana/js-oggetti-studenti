c2effee6a6b12fde63a91bf10f40b5850bbaded9
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-24 17:13:32 +0200

    fix indentatura

--- jsnack/jsnack3/js/main.js
+++ jsnack/jsnack3/js/main.js
@@ -9,46 +9,46 @@ Punti fatti e falli subiti
 */
 
  
-var squadre = [
-  {
-    nome: "milan",
-    puntiFatti: 0,
-    falliSubiti: 0
-  },
+  var squadre = [
+    {
+      nome: "milan",
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
 
-  {
-    nome: "juve",
-    puntiFatti: 0,
-    falliSubiti: 0
-  },
+    {
+      nome: "juve",
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
 
-  {
-    nome: "inter",
-    puntiFatti: 0,
-    falliSubiti: 0
-  },
+    {
+      nome: "inter",
+      puntiFatti: 0,
+      falliSubiti: 0
+    },
 
-  {
-    nome: "roma",
-    puntiFatti: 0,
-    falliSubiti: 0
+    {
+      nome: "roma",
+      puntiFatti: 0,
+      falliSubiti: 0
+    }
+  ]
+
+  for(var i = 0; i < squadre.length; i++){
+
+    squadre[i].puntiFatti = random();
+    squadre[i].falliSubiti = random();
+
   }
-]
 
-for(var i = 0; i < squadre.length; i++){
-
-  squadre[i].puntiFatti = random();
-  squadre[i].falliSubiti = random();
-
-}
-
-console.log(squadre);
+  console.log(squadre);
 
 
 
-function random(){
-  var random = Math.floor(Math.random()* 10)+1
-  return random;
+  function random(){
+    var random = Math.floor(Math.random()* 10)+1
+    return random;
 }
 
 
